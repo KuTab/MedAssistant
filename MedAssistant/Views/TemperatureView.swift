3dde147da5b957d5bb7f203ef48e689b6a64d36c
Egor Dadugin <mkrs95@yandex.ru>
2022-03-16 18:05:26 +0300

    add pickers

--- MedAssistant/Views/TemperatureView.swift
+++ MedAssistant/Views/TemperatureView.swift
@@ -8,31 +8,69 @@
 import Foundation
 import SwiftUI
 
+extension UIPickerView {
+    open override var intrinsicContentSize: CGSize { return CGSize(width: UIView.noIntrinsicMetric , height: super.intrinsicContentSize.height)}
+    
+}
+
 struct TemperatureView: View{
+    @State private var temperature: Int = 36
+    @State private var temperatureDecimal: Int = 6
+    
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View{
-        ZStack{
-            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.teal]), startPoint: .bottom, endPoint: .top).ignoresSafeArea(.all)
-            VStack{
-                Text("Temperature")
-                    .foregroundColor(.white)
-                    .font(.system(size: 60))
-                    .bold()
-                Spacer()
-                Button(action: doneMonitoring,
-                       label: {
-                    Text("Done")
-                        .font(.system(size: 30))
-                        .bold()
+        GeometryReader{ geometry in
+            ZStack{
+                LinearGradient(gradient: Gradient(colors: [Color.blue, Color.teal]), startPoint: .bottom, endPoint: .top).ignoresSafeArea(.all)
+                VStack{
+                    Text("Temperature")
                         .foregroundColor(.white)
-                        .padding()
-                        .overlay(RoundedRectangle(cornerRadius: 25).stroke(Color.white, lineWidth:  4))
-                        
+                        .font(.system(size: 60))
+                        .bold()
                     
-                })
-            }
-        }.navigationBarTitle("", displayMode: .inline)
+                    Spacer()
+                    
+                    HStack(spacing: 0){
+                        Picker("", selection: $temperature){
+                            ForEach(34..<41, id: \.self) { num in
+                                Text("\(num).")
+                                    .foregroundColor(.white)
+                            }
+                        }.pickerStyle(.wheel)
+                            .frame(width: geometry.size.width/3, height: 100)
+                            .compositingGroup()
+                            .clipped()
+                            .padding(.horizontal)
+                        
+                        Picker("", selection: $temperatureDecimal){
+                            ForEach(0..<10, id: \.self) { num in
+                                Text("\(num)")
+                                    .foregroundColor(.white)
+                            }
+                        }.pickerStyle(.wheel)
+                            .frame(width: geometry.size.width/3, height: 100)
+                            .compositingGroup()
+                            .clipped()
+                            .padding(.horizontal)
+                    }.overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.white, lineWidth: 4))
+                    
+                    Spacer()
+                    
+                    Button(action: doneMonitoring,
+                           label: {
+                        Text("Done")
+                            .font(.system(size: 30))
+                            .bold()
+                            .foregroundColor(.white)
+                            .padding()
+                            .overlay(RoundedRectangle(cornerRadius: 25).stroke(Color.white, lineWidth:  4))
+                        
+                        
+                    })
+                }
+            }.navigationBarTitle("", displayMode: .inline)
+        }
     }
     
     func doneMonitoring(){
--- MedAssistant/Views/WeightView.swift
+++ MedAssistant/Views/WeightView.swift
@@ -8,30 +8,63 @@
 import SwiftUI
 
 struct WeightView: View {
+    @State private var weight: Int = 75
+    @State private var weightDecimal: Int = 5
+    
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View{
-        ZStack{
-            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.teal]), startPoint: .bottom, endPoint: .top).ignoresSafeArea(.all)
-            VStack{
-                Text("Weight")
-                    .foregroundColor(.white)
-                    .font(.system(size: 60))
-                    .bold()
-                Spacer()
-                Button(action: doneMonitoring,
-                       label: {
-                    Text("Done")
-                        .font(.system(size: 30))
-                        .bold()
+        GeometryReader{geometry in
+            ZStack{
+                LinearGradient(gradient: Gradient(colors: [Color.blue, Color.teal]), startPoint: .bottom, endPoint: .top).ignoresSafeArea(.all)
+                VStack{
+                    Text("Weight")
                         .foregroundColor(.white)
-                        .padding()
-                        .overlay(RoundedRectangle(cornerRadius: 25).stroke(Color.white, lineWidth:  4))
+                        .font(.system(size: 60))
+                        .bold()
+                    Spacer()
                     
+                    HStack(spacing: 0){
+
+                        Picker("", selection: $weight){
+                            ForEach(0..<150, id: \.self) { num in
+                                Text("\(num).")
+                                    .foregroundColor(.white)
+                            }
+                        }.pickerStyle(.wheel)
+                            .frame(width: geometry.size.width/3, height: 100)
+                            .compositingGroup()
+                            .clipped()
+                            .padding(.horizontal)
+                        
+                        Picker("", selection: $weightDecimal){
+                            ForEach(0..<10, id: \.self) { num in
+                                Text("\(num)")
+                                    .foregroundColor(.white)
+                            }
+                        }.pickerStyle(.wheel)
+                            .frame(width: geometry.size.width/3, height: 100)
+                            .compositingGroup()
+                            .clipped()
+                            .padding(.horizontal)
+                    }.overlay(RoundedRectangle(cornerRadius: 20).stroke(Color.white, lineWidth: 4))
                     
-                })
-            }
-        }.navigationBarTitle("", displayMode: .inline)
+                    Spacer()
+                    
+                    Button(action: doneMonitoring,
+                           label: {
+                        Text("Done")
+                            .font(.system(size: 30))
+                            .bold()
+                            .foregroundColor(.white)
+                            .padding()
+                            .overlay(RoundedRectangle(cornerRadius: 25).stroke(Color.white, lineWidth:  4))
+                        
+                        
+                    })
+                }
+            }.navigationBarTitle("", displayMode: .inline)
+        }
     }
     
     func doneMonitoring(){
